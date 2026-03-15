9c39e866a1880bab768c58258cf84df239ffdcfb
stepi996 <167703773+stepi996@users.noreply.github.com>
2026-03-15 10:11:34 +0100

    Delete VENV directory

--- VENV/tempCodeRunnerFile.py
+++
@@ -1,6 +0,0 @@
-while not finished:
-    finished = functions.draw_path(constants.blocks)
-    if not finished:
-        print("Path generation failed; retrying")
-        constants.screen.fill(constants.blue)
-        constants.blocks = []
